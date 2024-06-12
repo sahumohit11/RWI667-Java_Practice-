462fdd393c74c7cc817699b38bfad651f18653e8
Mohit Sahu <95693541+sahumohit11@users.noreply.github.com>
2024-06-12 17:02:02 +0530

    MultiThreading Ques

---
+++ New folder/RunnableEx.java
@@ -0,0 +1,26 @@
+class Test implements Runnable{
+	@Override
+	public void run() {
+		for(int i=1; i<=10; i++) {
+			System.out.println(i+" test class");
+		}
+	}
+}
+public class RunnableEx{
+public static void main(String[] args) {
+	
+	Test t=new Test();
+	Test t1=new Test();
+	
+	Thread th=new Thread(t);
+	Thread th1=new Thread(t1);
+	th.setDaemon(true);
+	th.start();
+	th1.start();
+	
+	for(int i=1; i<=10; i++) {
+		System.out.println(i+" Main method");
+	}
+	
+}
+}
